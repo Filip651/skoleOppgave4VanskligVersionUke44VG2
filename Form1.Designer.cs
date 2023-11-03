fd588bdbb315f25fe75d2900f63c57debea8fc5b
Filip Melheim <filipmelh@viken.no>
2023-11-03 08:25:38 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace oppgave4VanskligVersion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblNumOfClicks = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.tCountDown = new System.Windows.Forms.Timer(this.components);
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.BackColor = System.Drawing.Color.Lime;
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(12, 112);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(254, 123);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Klikker";
+            this.btnClick.UseVisualStyleBackColor = false;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblNumOfClicks
+            // 
+            this.lblNumOfClicks.AutoSize = true;
+            this.lblNumOfClicks.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumOfClicks.Location = new System.Drawing.Point(43, 347);
+            this.lblNumOfClicks.Name = "lblNumOfClicks";
+            this.lblNumOfClicks.Size = new System.Drawing.Size(51, 55);
+            this.lblNumOfClicks.TabIndex = 1;
+            this.lblNumOfClicks.Text = "0";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeLeft.Location = new System.Drawing.Point(348, 347);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(51, 55);
+            this.lblTimeLeft.TabIndex = 2;
+            this.lblTimeLeft.Text = "0";
+            // 
+            // tCountDown
+            // 
+            this.tCountDown.Tick += new System.EventHandler(this.tCountDown_Tick);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(134, 347);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(183, 55);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Start på nytt";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Visible = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::oppgave4VanskligVersion.Properties.Resources.mouse;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(462, 468);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.lblNumOfClicks);
+            this.Controls.Add(this.btnClick);
+            this.Name = "Form1";
+            this.Text = "GameChanger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblNumOfClicks;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Timer tCountDown;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace oppgave4VanskligVersion
+{
+    public partial class Form1 : Form
+    {
+        bool bStarted = false; // Flag to track if the countdown has started
+        private int countdown = 10; // Initialize the countdown variable to 10
+        private int numOfClicks = 0; // Initialize the number of clicks to 0
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Event handler for form load
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            if (!bStarted)
+            {
+                lblTimeLeft.Text = countdown.ToString(); // Display the countdown value
+                tCountDown.Interval = 1000; // Set the interval to 1000 milliseconds (1 second)
+                tCountDown.Start(); // Start the countdown timer
+            }
+
+            numOfClicks++; // Increment the number of clicks
+            lblNumOfClicks.Text = numOfClicks.ToString(); // Display the number of clicks
+        }
+
+        private void tCountDown_Tick(object sender, EventArgs e)
+        {
+            if (countdown > 0)
+            {
+                countdown--; // Decrement the countdown value
+                lblTimeLeft.Text = countdown.ToString(); // Display the updated countdown value
+            }
+            else
+            {
+                tCountDown.Stop(); // Stop the countdown timer
+                btnClick.Enabled = false; // Disable the click button
+                btnReset.Visible = true; // Make the reset button visible
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            btnClick.Enabled = true; // Enable the click button
+            btnReset.Visible = false; // Hide the reset button
+            bStarted = false; // Reset the started flag
+            countdown = 10; // Reset the countdown value to 10
+            numOfClicks = 0; // Reset the number of clicks to 0
+        }
+    }
+}
